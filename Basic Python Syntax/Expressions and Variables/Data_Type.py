8df1191d616e8a1a5a279d220b38c0c7b09b30a9
Chen135-nbc <3042652047@qq.com>
2025-11-14 14:36:24 +0800

    Initial commit

---
+++ Basic Python Syntax/Expressions and Variables/Data_Type.py
@@ -0,0 +1,19 @@
+# How to annotate a variable
+name: str = "Betty"
+age: int = 10
+print(name,age)
+
+# Expressions,numbers, and type conversions
+# 整型相加
+a = 7 + 8
+print(a)
+# float
+b = 8.6 * 9.3
+print(b)
+# 字符串相加打印
+print("This " + "is " + "a programs")
+# 同时打印字符串和数字，需要把数字转化为字符串
+base = 6
+height = 3
+area = (base * height) / 2
+print("The area of the triangle is", str(area))
---
+++ Basic Python Syntax/Expressions and Variables/Expressions_and_variables.py
@@ -0,0 +1,16 @@
+import typing
+x : str = "Hello World!"
+y : int = 3
+z : float = 3.14
+
+# List
+list_of_numbers : typing.List[int] = [1, 2, 3]
+
+# tuple
+tuple_of_numbers : typing.Tuple[int, int, int] = (1, 2, 3)
+
+# Dictionary
+dictionary : typing.Dict[str, int] = { "key1" : 1, "key2" : 2 }
+
+# set
+set_of_numbers : typing.Set[int] = {1, 2, 3}
