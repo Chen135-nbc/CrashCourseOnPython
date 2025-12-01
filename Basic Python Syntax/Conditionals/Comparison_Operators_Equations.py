4d0ec79b594466d77ef7b660b6964fca5d93c968
Chen135-nbc <3042652047@qq.com>
2025-12-01 15:29:18 +0800

    Basic python Syntax/Conditions/Comparison Operators with Strings

---
+++ Basic Python Syntax/Conditionals/Comparison_Operators_Equations.py
@@ -0,0 +1,30 @@
+# PART 1: Equality == and Not Equal To != Operators
+
+print(32 == 30+2) #return True
+print(10+8 == 9+7) #return False
+print(10-4 != 10+4)
+print(9/3 != 3*1)
+
+# The equality == operator versus the equals = operator
+# equals operator "="
+my_variable = 3 * 5
+print(my_variable)
+# equality operator "=="
+print(my_variable == 3*5)
+
+# PART 2: Greater Than > and Less Than < Operators
+# > Greater Than
+print(11 > 3*3)  #True
+print(4/2 > 8-4) #False
+
+# < Less Than
+print(4/2 < 8/4) #False
+print(11 < 3*2) #False
+
+# PART 3: Greater Than or Equal to >= and Less Than or Equal to <= Operators
+# Greater Than or Equal to >=
+print(12*2 >= 24)
+print(18/2 >= 15)
+# Less Than or Equal to <=
+print(12*2 <= 30)
+print(15 <= 18/2)
---
+++ Basic Python Syntax/Conditionals/Comparison_Operators_String.py
@@ -0,0 +1,25 @@
+# PART 1: Equality == and Not Equal to != Operators with Strings
+print("a string" == "a string")
+print("4 + 5" == 4 + 5)     #  the two values cannot be equal
+print("rabbit" != "frog")
+
+event_city = "Shanghai"
+print(event_city != "Shanghai")
+
+print("three" == 3)
+
+# PART 2: The Greater Than > and Less Than < Operators
+# 大写字母的ASCII码值 < 小写字母的ASCII码值
+print("Wednesday" > "Saturday")
+print("Brown" < "brown")
+print("sunbathe" > "suntan")
+print("Lima" < "Lima")
+
+#PART 3: The Greater Than or Equal To >= and Less Than or Equal To <= Operators
+var1 = "my computer" >= "my chair"
+var2 = "Spring" <= "Winter"
+var3 = "pineapple" >= "pineapple"
+
+print("Is \"my computer\" greater than or equal to \"my chair\"? Result: ", var1)
+print("Is \"Spring\" less than or equal to \"Winter\"? Result: ", var2)
+print("Is \"pineapple\" less than or equal to \"pineapple\"? Result: ", var3)
